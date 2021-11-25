8c9c5ed96c9b49678dd0bef6e1de3da30ca0dec8
funakattila <funak.attila@gmail.com>
2021-11-25 22:00:24 +0100

    refine create_tables.sql and queries.sql

--- create_tables.sql
+++ create_tables.sql
@@ -16,9 +16,9 @@ CREATE TYPE solo_or_team AS ENUM ('solo', 'team');
 -- courses table
 CREATE TABLE courses (
     course_id serial PRIMARY KEY,
-    course_title varchar(50),
-    course_summary text,
-    start_course date
+    course_title varchar(50) NOT NULL,
+    course_summary text NOT NULL,
+    start_course date NOT NULL
 );
 
 -- projects table
@@ -26,15 +26,15 @@ CREATE TABLE projects (
     project_id serial PRIMARY KEY,
     project_title varchar(100),
     project_summary text,
-    exercise_or_tutorial exercise_or_tutorial,
-    solo_or_team solo_or_team
+    exercise_or_tutorial exercise_or_tutorial NOT NULL,
+    solo_or_team solo_or_team NOT NULL
 );
 
 -- courses_projects tabel
 CREATE TABLE courses_projects (
     courses_projects_id serial PRIMARY KEY,
-    course_id int,
-    project_id int,
+    course_id int NOT NULL,
+    project_id int NOT NULL,
     CONSTRAINT fk_course_id_1 FOREIGN KEY (course_id)
     REFERENCES courses(course_id),
     CONSTRAINT fk_project_id_1 FOREIGN KEY (project_id)
@@ -44,9 +44,9 @@ CREATE TABLE courses_projects (
 -- tasks table
 CREATE TABLE tasks (
     task_id serial PRIMARY KEY,
-    project_id int,
-    task_title varchar(100),
-    task_summary text,
+    project_id int NOT NULL,
+    task_title varchar(100) NOT NULL,
+    task_summary text NOT NULL,
     CONSTRAINT fk_project_id_2 FOREIGN KEY (project_id)
     REFERENCES projects(project_id)
 );
@@ -54,7 +54,7 @@ CREATE TABLE tasks (
 -- background materials table
 CREATE TABLE background_materials (
     background_material_id serial PRIMARY KEY,
-    task_id int,
+    task_id int NOT NULL,
     background_material_title varchar(100),
     background_material_url varchar(100),
     CONSTRAINT fk_task_id FOREIGN KEY (task_id)
@@ -64,12 +64,12 @@ CREATE TABLE background_materials (
 -- students table
 CREATE TABLE students (
     student_id serial PRIMARY KEY,
-    course_id int,
-    student_name varchar(50),
+    course_id int NOT NULL,
+    student_name varchar(50) NOT NULL,
     nick_name varchar(50),
     city varchar(50),
     hobby varchar(50),
-    email varchar(50),
+    email varchar(50) UNIQUE NOT NULL,
     CONSTRAINT fk_course_id_3 FOREIGN KEY (course_id)
     REFERENCES courses(course_id)
 );
@@ -77,7 +77,7 @@ CREATE TABLE students (
 -- reviewers table
 CREATE TABLE reviewers (
     reviewer_id serial PRIMARY KEY,
-    student_id int,
+    student_id int NOT NULL,
     CONSTRAINT fk_student_id_1 FOREIGN KEY (student_id)
     REFERENCES students(student_id)
 );
@@ -85,11 +85,11 @@ CREATE TABLE reviewers (
 -- review table
 CREATE TABLE reviews (
     review_id serial PRIMARY KEY,
-    project_id int,
-    reviewer_id int,
-    student_id int,
-    evaluation int,
-    evaluation_date date,
+    project_id int NOT NULL,
+    reviewer_id int NOT NULL,
+    student_id int NOT NULL,
+    evaluation int NOT NULL,
+    evaluation_date date DEFAULT now(),
     evaluation_summary text,
     CONSTRAINT fk_project_id_4 FOREIGN KEY (project_id)
     REFERENCES projects(project_id),
--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
+-- Hozz létre olyan módosító lekérdezéseket, amikkel adott esetben egy-egy értékelés eredményét módosítani lehet, illetve a hibás értékelés eredménye akár törölhető is. Az utasítások nem kell, hogy paraméterezettek legyenek, tetszőleges tesztadatot használj a módosításkor, illetve törléskor.
+
+UPDATE reviews -- Az értékelés frissítése
+SET evaluation = 5
+WHERE review_id = 1
+
+
+
+DELETE FROM reviews -- Az értékelés törlése
+WHERE review_id = 2
+
 
 SELECT -- 5.a | A tananyag kilistázása kurzusonként
        courses.course_title,
@@ -6,9 +17,10 @@ FROM courses
 INNER JOIN courses_projects ON courses.course_id = courses_projects.course_id
 INNER JOIN projects ON courses_projects.project_id = projects.project_id
 
-WHERE courses.course_id = 1
+WHERE courses.course_id = 2
 
 
+-- **************** MÉG NEM TELJESEN JÓ *****************
 
 SELECT -- 5.b | Az egyes projektek megjelenítése azok minden elemével együtt
        projects.project_title,
@@ -16,16 +28,19 @@ SELECT -- 5.b | Az egyes projektek megjelenítése azok minden elemével együtt
        projects.exercise_or_tutorial,
        projects.solo_or_team,
        tasks.task_title,
-       tasks.task_summary,
-       background_materials.background_material_title,
-       background_materials.background_material_url
+       tasks.task_summary
+       --background_materials.background_material_title,
+       --background_materials.background_material_url
+FROM projects
+INNER JOIN tasks ON projects.project_id = tasks.project_id
+--INNER JOIN background_materials ON tasks.task_id = background_materials.task_id
+
+WHERE projects.project_id = 3
+
+SELECT *
 FROM projects
 INNER JOIN tasks ON projects.project_id = tasks.project_id
 INNER JOIN background_materials ON tasks.task_id = background_materials.task_id
-
-WHERE projects.project_id = 1
-
-
 
 SELECT -- 5.c | A diákok alapadatainak listája kurzusonként
        courses.course_title,
@@ -52,8 +67,26 @@ INNER JOIN reviews ON projects.project_id = reviews.project_id
 INNER JOIN reviewers ON reviews.reviewer_id = reviewers.reviewer_id
 INNER JOIN students ON reviewers.student_id = students.student_id
 
-WHERE students.student_id = 2
+WHERE students.student_id = 3
 
 
--- Összesített eredmény  a diákok teljesítéseiről. A lekérdezés számoljon átlagot az elért pontszámokból és ha a diák 60% felett teljesített, kapjon “haladó” minősítést, ha alatta, legyen “megfontolva haladó”.
--- A 3 legjobban teljesítőtelsesítő diák listája az összesített (nem átlag) pontszámaik alapján. 
+
+SELECT -- Összesített eredmény  a diákok teljesítéseiről. A lekérdezés számoljon átlagot az elért pontszámokból és ha a diák 60% felett teljesített, kapjon “haladó” minősítést, ha alatta, legyen “megfontolva haladó”.
+students.student_name,
+CASE WHEN AVG(reviews.evaluation) / 5 > 0.6 THEN 'haladó'
+     ELSE 'megfontolva haladó'
+END AS "előrehaladás"
+FROM students
+INNER JOIN reviews ON students.student_id = reviews.student_id 
+GROUP BY 1
+ORDER BY 1
+
+
+
+SELECT -- A 3 legjobban teljesítőtelsesítő diák listája az összesített (nem átlag) pontszámaik alapján. 
+students.student_name,
+SUM(reviews.evaluation)
+FROM students
+INNER JOIN reviews ON students.student_id = reviews.student_id 
+GROUP BY 1
+ORDER BY 2 DESC, 1
